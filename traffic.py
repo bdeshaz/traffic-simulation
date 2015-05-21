a338506208c98f8023e560e57b3d74f555988179
Brian <bdeshaz@gmail.com>
2015-05-21 16:22:03 -0400

    added attributes

---
+++ traffic.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+## Classes
+## Car
+class Car:
+#   Attributes
+    def __init__(self,
+                    starting_position,
+                    length=0,
+                    desired_speed=3.33,
+                    other_car):
+
+#       - starting position
+#       - car length
+#       - desired speed
+#       - other (position & speed)
+#   Methods
+#       -current position: property??
+#       -accelerate: if current speed as distance > distance b/w other car,
+#           accelerate @ 2m/s as long as there is room
+#       -measures distance b/w other car. Needs location of other car
+#       -slow down: if distance is current speed as distance < distance b/w car,
+#           match other car's speed
+#       -stop: if moving results in colision, stop = speed = 0
+#       -randomly slow down: every second, randomly choose to slow down by 2m/s
+#        def next_position(self):
+#           x_move = self.speed
+#           return (self.x + x_move)
+
+
+## Road
+#   Attributes
+#       -length of road
+#   Methods:
+
+
+## Simulation
+#   Attributes:
+#       -cars
+#       -Road
+#       -captured speed of all cars [numpy array]
+#
+#   Methods:
+#       -reset location when car reaches end of Road
+#       -set spacing [numpy array]
+#       -create 30 cars [a list]
+#       -moves cars
+#       -start time
+#       -capture speed of all cars at each second
+#       -end time
